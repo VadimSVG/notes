f330fb15640fdd5b9450ec0ce139b953f73e9401
VadimSVG <dimasw99@gmail.com>
2023-10-18 11:43:05 +0300

    Second version

--- src/main/java/vadimsw/notes/controllers/NotesController.java
+++ src/main/java/vadimsw/notes/controllers/NotesController.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import vadimsw.notes.models.Note;
 import vadimsw.notes.repo.NoteRepository;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class NotesController {
@@ -28,6 +33,44 @@ public class NotesController {
                               @RequestParam String full_text, Model model){
         Note note = new Note(title,anons,full_text);
         noteRepository.save(note);
+        return "redirect:/notes-main";
+    }
+    @GetMapping("/notes-main/{id}")
+    public String notesDetails(@PathVariable(value = "id") long id, Model model){
+        if (!noteRepository.existsById(id)){
+            return "redirect:/notes-main";
+        }
+       Optional<Note> note = noteRepository.findById(id);
+        ArrayList<Note> res = new ArrayList<>();
+        note.ifPresent(res::add);
+       model.addAttribute("note", res);
+        return "notes-details";
+    }
+    @GetMapping("/notes-main/{id}/edit")
+    public String notesEdit(@PathVariable(value = "id") long id, Model model){
+        if (!noteRepository.existsById(id)){
+            return "redirect:/notes-main";
+        }
+        Optional<Note> note = noteRepository.findById(id);
+        ArrayList<Note> res = new ArrayList<>();
+        note.ifPresent(res::add);
+        model.addAttribute("note", res);
+        return "notes-edit";
+    }
+    @PostMapping("/notes-main/{id}/edit")
+    public String notePostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons,
+                              @RequestParam String full_text, Model model){
+        Note note = noteRepository.findById(id).orElseThrow();
+        note.setTitle(title);
+        note.setAnons(anons);
+        note.setFull_text(full_text);
+        noteRepository.save(note);
+        return "redirect:/notes-main";
+    }
+    @PostMapping("/notes-main/{id}/remove")
+    public String notePostDelete(@PathVariable(value = "id") long id, Model model){
+        Note note = noteRepository.findById(id).orElseThrow();
+        noteRepository.delete(note);
         return "redirect:/notes-main";
     }
 
